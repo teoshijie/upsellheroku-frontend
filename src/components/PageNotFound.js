59c1564e0a0f6a953aa1800b39d3c19e61228ce2
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2020-07-31 22:51:26 +0800

    product listing done

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -10,3 +10,4 @@ class PageNotFound extends Component {
 }
  
 export default PageNotFound;
+
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {ListingConsumer} from '../context';
+
 
 
 
@@ -11,19 +11,24 @@ class Product extends Component {
         return (
             
             <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                <div className='className="card'>
+                <div className="card" style={{width:"15rem", marginBottom:"15px"}}>
                      <div 
-                        className='img-container p-5' 
+                        className='img-container p-5 view overlay zoom' 
                         onClick={() => console.log('you clicked me on the image container')}>
                         <Link to="/details">
                             <img src={image_url} alt='' className="card-img-top"/>
                         </Link>
                     </div>
-                    <div className="card-body">
+                    <div className="card-body mx-auto">
                         <h5 className="card-title">{name}</h5>
                         <p className="card-text">${price.$numberDecimal}</p>
                         <p className="card-text">{condition}</p>
+                    <Link to='/buynow'>
+                        <button type='button' className="btn btn-danger">Buy Now</button>
+                    </Link>
                     </div>
+                    
+                    
                 </div>
 
             </div>
@@ -37,18 +42,3 @@ class Product extends Component {
 }
  
 export default Product;
-
-
-
-// <div className="card" style={{width:"15rem", marginBottom:"15px"}}>
-                
-                
-// <img src={image} alt='listing' className="card-img-top ml-3" style={{width:"200px", height:"200px"}} />
-// <div className="card-body">
-//     <h5 className="card-title">{name}</h5>
-//     <p className="card-text">${price}</p>
-//     <p className="card-text">{condition}</p>
-// </div>
-
-
-// </div>
