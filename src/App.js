c1021f8071d4260cde40dc47492e1a2cd04d1b6f
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2020-08-04 10:30:36 +0800

    Sell_can't add without refresh & HOCS_not works

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import ProductListing from './components/ProductListing';
 import Details from './components/Details';
 import BuyNow from './components/BuyNow';
+import Sell from './components/Sell';
 import EditListing from './components/EditListing';
 import PageNotFound from './components/PageNotFound';
 import Login from './components/authentication/login';
@@ -14,6 +15,9 @@ import Signup from './components/authentication/signup';
 import SignUpSucess from './components/authentication/SignUpSuccess';
 import Profile from './components/profile';
 import {BrowserRouter as Router} from 'react-router-dom';
+import PrivateRoute from './hocs/PrivateRoute';
+import UnPrivateRoute from './hocs/UnPrivateRoute';
+
 
 class App extends Component{
 
@@ -24,13 +28,17 @@ class App extends Component{
           <Switch>
             <Route exact path="/" component={ProductListing} />
             <Route path="/:itemID/details" component={Details} />
-            <Route path="/:itemID/buynow" component={BuyNow} />
-            <Route path='/editlisting' component={EditListing} />
-            <Route path = "/login" component = {Login}/>
+            
+            <privateRoute path="/:itemID/buynow" component={BuyNow} />
+            <PrivateRoute path="/sell" roles={["user"]} component={Sell} />
+            <PrivateRoute path="/editlisting" component={EditListing} />
+            <PrivateRoute path="/profile" component={Profile} />
+
+            <UnPrivateRoute path="/login" component={Login}/>
             <Route path = "/loginsuccess" component = {LoginSuccess}/>
-            <Route path = "/signup" component = {Signup}/>
+            <UnPrivateRoute path = "/signup" component={Signup}/>
             <Route path = "/signupsucess" component = {SignUpSucess}/>
-            <Route path = "/profile" component = {Profile}/>
+            
             <Route component={PageNotFound} />
           </Switch>       
       </React.Fragment>
@@ -40,3 +48,4 @@ class App extends Component{
 }
 
 export default App;
+
